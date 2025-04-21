cdb804fa3cb0b3af8e9f6b237737fe3954557d69
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2025-04-20 20:48:33 -0400

    up to CH2L5

--- gator.go
+++ gator.go
@@ -1,19 +1,74 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/englishlayup/gator/internal/config"
+	"github.com/englishlayup/gator/internal/database"
+	"github.com/google/uuid"
 )
+
+func handlerReset(s *state, cmd command) error {
+	ctx := context.Background()
+	return s.db.DeleteUsers(ctx)
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("Expect a single argument, the username.")
+	}
+
+	name := cmd.args[0]
+	currentTime := time.Now()
+	ctx := context.Background()
+
+	user, err := s.db.CreateUser(
+		ctx,
+		database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: sql.NullTime{Time: currentTime, Valid: true},
+			UpdatedAt: sql.NullTime{Time: currentTime, Valid: true},
+			Name:      sql.NullString{String: name, Valid: true},
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("User %v created", user.Name.String)
+
+	if err := s.cfg.SetUser(user.Name.String); err != nil {
+		return err
+	}
+	return nil
+}
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("Expect a single argument, the username.")
 	}
+	ctx := context.Background()
+	name := cmd.args[0]
 
-	err := s.cfg.SetUser(cmd.args[0])
+	_, err := s.db.GetUser(
+		ctx,
+		sql.NullString{
+			String: name,
+			Valid:  true,
+		},
+	)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to login as %v\n", name)
+		log.Fatal(err)
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
 		return err
 	}
 
@@ -32,15 +87,11 @@ func (c *commands) run(s *state, cmd command) error {
 		return fmt.Errorf("Command '%v' not registered", cmd.name)
 	}
 
-	err := handler(s, cmd)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return handler(s, cmd)
 }
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
--- main.go
+++ main.go
@@ -1,34 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/englishlayup/gator/internal/config"
+	"github.com/englishlayup/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
-
 func main() {
-    userConfig, err := config.Read()
-    if err != nil {
-        log.Fatal(err)
-    }
-    gatorState := state{ &userConfig }
+	userConfig, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := sql.Open("postgres", userConfig.DbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+	gatorState := state{
+		dbQueries,
+		&userConfig,
+	}
 
-    commands := commands { make(map[string]func(*state, command) error) }
+	commands := commands{make(map[string]func(*state, command) error)}
 
-    commands.register("login", handlerLogin)
-    args := os.Args
-    if len(args) < 2 {
-        log.Fatal("Usage: gator login <username>")
-    }
+	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
+	commands.register("reset", handlerReset)
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("Usage: gator [login|register] <username>")
+	}
 
-    command := command {
-        name: "login",
-        args: args[2:],
-    }
+	command := command{
+		name: args[1],
+		args: args[2:],
+	}
 
-    if err := commands.run(&gatorState, command); err != nil {
-        log.Fatal(err)
-    }
+	if err := commands.run(&gatorState, command); err != nil {
+		log.Fatal(err)
+	}
 }
